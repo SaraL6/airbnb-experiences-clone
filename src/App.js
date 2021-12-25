5dc59fa3e964194610f3b5419a668d3cdbe51c7f
SaraL6 <71009666+SaraL6@users.noreply.github.com>
2021-12-25 18:26:39 +0100

    Card data fetching with props

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import Card from "./components/Card"
+import cardData from "../src/data"
 
 
 /*
@@ -11,11 +12,21 @@ Check the Figma file for the design specifics.
 */
 
 export default function App() {
+    const cards = cardData.map(data => {
+        return <Card
+            key={data.id}
+            {...data}
+
+        />
+    })
+
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <section className="cards-list">
+                {cards}
+            </section>
         </div>
 
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-/*
-Challenge: Build the Card component
-For now, hard-code in the data (like 
-the rating, title, price, etc.)
+import Cardstar from "../images/Star 1.png"
 
-Notes:
-- Only render 1 instance (I already did this for you)
-- The star icon and photo (katie-zaferes.png) are in the images 
-  folder for your use
-- Make sure to include:
-    - image
-    - star icon (star.png), rating, and review count
-    - title
-    - cost/person
-- The main purpose of this challenge is to show you where our limitations
-  currently are, so don't worry about the fact that you're hard-coding all
-  this data into the component.
-*/
 
-export default function Card() {
+export default function Card(props) {
+
+    let badgeText;
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+
     return (
-        <h1>Replace this</h1>
+
+        <section className="card">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${props.coverImg}`} alt="" className="card--image" />
+            <div className="card--stats">
+                <img src={Cardstar} alt="" className="card--star" />
+                <span>{props?.stats?.rating} </span>
+                <span className="gray">({props?.stats?.reviewCount}) . </span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"> <span className="bold"> From ${props.price} /</span> person</p>
+        </section>
+
     )
+
 }
 
 ReactDOM.render(<Card />, document.getElementById("root"))
